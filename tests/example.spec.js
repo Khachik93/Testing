9540ba76b52e9f34fbbd466d51b1f8ca7a971967
Khachik <khachik.matevosyan.im@gmail.com>
2023-01-11 16:40:10 +0400

    testing buy.am buy product

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,8 +3,10 @@ const { test, expect } = require('@playwright/test');
 
 test('test', async ({ page }) => {
   await page.goto('https://buy.am/');
-  await page.getByRole('link', { name: 'Սուպերմարկետ ' }).click();
-  await page.getByRole('link', { name: 'Քարֆուր' }).click();
-  await expect(page).toHaveURL('https://buy.am/hy/supermarket/carrefour')
+  await page.getByRole('link', { name: '0 ' }).click();
+  await page.getByRole('link', { name: '\nՎերադառնալ ' }).click();
+  await page.locator('div:nth-child(109) > .emotion--product-slider > .product-slider > .product-slider--container > div:nth-child(6) > .product--box > .box--content > .product--info > .product--details > .product-actions > .product--btn-container > .buybox--form > .buybox--button').click();
+  await page.getByRole('link', { name: '1 ' }).click();
+  await page.getByRole('button', { name: '' }).click();
 });
 
