151aa1b72f454b575bf0f2d9cf42f105ec30fa74
Khachik <khachik.matevosyan.im@gmail.com>
2023-02-08 19:11:55 +0400

    task

--- tests/menu.js
+++ tests/menu.js
@@ -1,19 +1,22 @@
+const { expect } = require('@playwright/test')
 exports.PlaywrightDevPage = class PlaywrightDevPage {
 
 constructor(page) {
     this.page = page;
-    // this.getSingInBtn = page.locator("header > div[class^=MuiContainer-root] > div[class^=MuiGrid-root] > div[class=MuiBox-root] > div[class=MuiBox-root] > button[class=MuiButtonBase-root] > span[class=MuiButton-label]");
     this.getSingInBtn = page.getByRole('button', { name:'Sign In' });
-    this.loginWindow = page.locator('div').toHaveClass('MuiPaper-root MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthXs MuiDialog-paperFullWidth MuiPaper-elevation24 MuiPaper-rounded')
+    this.loginWindow = page.getByTitle('Մուտք');
+  
   }
 
   async goto() {
-    await this.page.goto('https://menu.am/');
+    await this.page.goto('https://menu.am/',{ waitUntil: 'networkidle' });
     console.log('GOTO')
    
   }
   async getStartedSingIn() {
     await this.getSingInBtn.click();
-    await expect(this.loginWindow).isDisabled();
+    console.log("click true ==== ")
+    await expect(this.loginWindow).toBeDisabled();
+    console.log("display")
   }
 }
