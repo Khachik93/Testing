714e391126eb50dbebade8a1bab6bd34ffc6903a
Khachik <khachik.matevosyan.im@gmail.com>
2023-02-14 19:42:19 +0400

    for lesson

--- lessonNode/index.js
+++ lessonNode/index.js
@@ -1,15 +1,37 @@
-const fs = require('fs');
+import express from "express";
+import path from "path";
+import fs from "fs"
 
 
-const rates = fs.readFileSync('./files/rates.csv', 'utf8');
-const toArrayrates = rates.split('\n');
-// const ff = `{${toArrayrates[0]}}`
-// console.log(ff);
-const personalInfo = fs.readFileSync('./files/personalInfo.csv', 'utf8');
-const toArrayInfo = personalInfo.split('\n');
- for(let i= 0;i<toArrayInfo.length;i++){
-    // toArrayInfo[i].JSON.parse()
-    // toArrayrates[i].JSON.parse()
-    console.log(toArrayInfo[i],toArrayrates[i]);
- }
+const app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("files/fullInfo.json"))
+
+})
+app.post('/', (req, res) => {
+    const fullInfo = fs.readFileSync('./files/fullInfo.json', 'utf8');
+    const myfullInfo = JSON.parse(fullInfo);
+    myfullInfo.push({
+        "id": 8,
+        "firstName": "Khachik",
+        "lastName": "Matevosyan",
+        "address": "Address1",
+        "phoneNumber": "PhoneNumber1",
+        "email": "Email1",
+        "rates": {
+            "Mathemathics": 5,
+            "English": 2,
+            "Physics": 3,
+            "Geography": 6,
+            "Algorithms": 2
+        }
+    })
+    const newInfo = JSON.stringify(myfullInfo);
+    fs.writeFileSync('files/fullInfo.json', newInfo);
+    res.send("user successfully created");
+})
+app.use('/ping', (request, response) => {
+    response.send('pong');
+});
+app.listen(3001)
