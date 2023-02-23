b796a48bea95fb678ac072bc4d3d225316a11f6a
Khachik <khachik.matevosyan.im@gmail.com>
2023-02-23 10:23:10 +0400

    home jest

---
+++ node_api_1/jest.config.js
@@ -0,0 +1,15 @@
+const config = {
+    verbose: true,
+    moduleFileExtensions: [
+        "js",
+        "json",
+      ],
+      automock: false,
+      reporters: undefined,
+      testURL: "http://localhost",
+      transformIgnorePatterns: [
+        "/node_modules/"
+      ],
+  };
+  
+  module.exports = config;
