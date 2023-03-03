cfeb1f869795717b352370a5d72d66672704a568
Khachik <khachik.matevosyan.im@gmail.com>
2023-03-03 11:02:12 +0400

    home playwright

---
+++ lessonPlaywrightTest/pages/lpogin.page.js
@@ -0,0 +1,21 @@
+const { expect } = require('@playwright/test');
+
+exports.LoginPage = class LoginPage {
+
+  constructor(page) {
+    this.myAccount = page.locator('#widget-navbar-217834 > ul > li:nth-child(6)'),
+      this.loginButton = page.locator('#widget-navbar-217834 > ul > li:nth-child(6) > ul > li:nth-child(1) > a'),
+      this.loginContainer = page.locator('#content > div > div:nth-child(2) > div > div'),
+      this.loginContainerTitle = page.locator('#content > div > div:nth-child(2) > div > div > h2'),
+      this.newCustomerContainer = page.locator('#content > div > div:nth-child(1) > div > div'),
+      this.newCustomerTitle = page.locator('#content > div > div:nth-child(1) > div > div > h2')
+  }
+
+  async hoverElement() {
+    await this.myAccount.hover()
+    await expect(this.loginButton).toBeVisible();
+  }
+  async clickMyAccount() {
+    await this.loginButton.click();
+  }
+}
--- lessonPlaywrightTest/tests/example.spec.js
+++ lessonPlaywrightTest/tests/example.spec.js
@@ -1,24 +1,21 @@
 
 const { test, expect } = require('@playwright/test');
+const { Page } = require('../pages/page');
+const { LoginPage } = require('../pages/lpogin.page');
+
+
 test.beforeEach(async ({ page }, testInfo) => {
-  await page.goto('https://ecommerce-playground.lambdatest.io/', { waitUntil: 'networkidle' });
+  const mainPage = new Page(page);
+  await mainPage.goto();
   await expect(page).toHaveTitle(/Your Store/);
 });
 
 
 test('Verify the my account field functionality @C8', async ({ page }) => {
-  await page.locator('#widget-navbar-217834 > ul > li:nth-child(6)').hover();
-  const locator = page.locator('#widget-navbar-217834 > ul > li:nth-child(6) > ul > li:nth-child(1) > a');
-  await expect(locator).toBeVisible();
-  await page.locator('#widget-navbar-217834 > ul > li:nth-child(6) > ul > li:nth-child(1) > a').click();
-  await expect(page).toHaveURL('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
-  const loginContainer = page.locator('#content > div > div:nth-child(2) > div > div');
-  await expect(loginContainer).toBeVisible();
-  const loginContainerTitle = page.locator('#content > div > div:nth-child(2) > div > div > h2');
-  await expect(loginContainerTitle).toHaveText(/Returning Customer/);
-  const newCustomerContainer = page.locator('#content > div > div:nth-child(1) > div > div');
-  await expect(newCustomerContainer).toBeVisible();
-  const newCustomerTitle = page.locator('#content > div > div:nth-child(1) > div > div > h2');
-  await expect(newCustomerTitle).toHaveText(/New Customer/);
-})
-
+  const loginPage = new LoginPage(page);
+  await loginPage.hoverElement();
+  test.step("click login button", async () => {
+  await loginPage.clickMyAccount();
+  
+  })
+});
