4d91745d72c6221d737c7564d2df10104a59ad48
Khachik <khachik.matevosyan.im@gmail.com>
2023-03-23 16:00:02 +0400

    proxy

--- designPatterns/proxy.py
+++ designPatterns/proxy.py
@@ -1,28 +1,33 @@
 from abc import ABC, abstractmethod
-
 class Bank(ABC):
-    @abstractmethod
-    def loan_repayment(self):
+    def varki_marum(self):
         pass
 
-class Unibank(Bank):
-    def loan_repayment(self):
-        print("payment is made")        
+class Acba(Bank):
+    def varki_marum(self):
+        print("varki marum")
 
 class Proxy(Bank):
-    def __init__(self, user):
-        self.user = user
 
-    def check_access(self):
-        return True    
 
-    def loan_repayment(self):
+    def __init__(self, real_bank: Bank) -> None:
+        self._real_bank = real_bank
+
+    def check_access(self) -> bool:
+        print("Proxy: Checking access prior to firing a real request.")
+        return True
+
+    def varki_marum(self):
         if self.check_access():
-            print("ok")   
+            print("pass")
+            self._real_bank.varki_marum()
 
-    
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    acba = Acba()
+    acba.varki_marum()
 
-convers_bank = Unibank()
-proxy = Proxy(convers_bank)
-proxy.loan_repayment()       
+    vtb = Acba()
+    proxy = Proxy(vtb)
+    proxy.varki_marum()
